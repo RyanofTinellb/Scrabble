da11791b364f07ae257afcbc908547f23448d729
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-07-04 12:02:34 +1000

    Added tests for bonus string

--- scrabble_spec.rb
+++ scrabble_spec.rb
@@ -3,12 +3,22 @@ require_relative "Scrabble"
 RSpec.describe Scrabble do
 	describe '#score' do
 		it 'returns the score of a word' do
-			expect(Scrabble.new.score("hello")).to eq(8)
-			expect(Scrabble.new.score("cabbage")).to eq(14)
-			expect(Scrabble.new.score("Hello")).to eq(8)	# capitalisation
-			expect(Scrabble.new.score("")).to eq(0)		# empty string
-			expect(Scrabble.new.score(nil)).to eq(0)	# nil input
-			expect{Scrabble.new.score("he5llo")}.to raise_error(ArgumentError, "illegal characters in word")		#illegal characters
+			game = Scrabble.new
+			expect(game.score("hello")).to eq(8)
+			expect(game.score("cabbage")).to eq(14)
+			expect(game.score("Hello")).to eq(8)	# capitalisation
+			expect(game.score("")).to eq(0)		# empty string
+			expect(game.score(nil)).to eq(0)	# nil input
+			expect{game.score("he5llo")}.to raise_error(ArgumentError, "illegal characters in word")
+			expect(game.score("hello", "-2---")).to eq(9)	# double letter score
+			expect(game.score("hello", "-3---")).to eq(10)	# triple letter score
+			expect(game.score("hello", "-D---")).to eq(16)	# double word score
+			expect(game.score("hello", "-T---")).to eq(24)	# triple word score
+			expect(game.score("hello", "-2-D-")).to eq(18)	# double word score and double letter score
+			expect(game.score("hello", "-D-T-")).to eq(48)	# double word score and triple word score
+			expect{game.score("hello", "---")}.to raise_error(ArgumentError, "too few characters in bonus string")
+			expect{game.score("hello", "--------")}.to raise_error(ArgumentError, "too many characters in bonus string")
+			expect{game.score("hello", "-R---")}to raise_error(ArgumentError, "illegal characters in bonus string")
 		end
 	end
 end
