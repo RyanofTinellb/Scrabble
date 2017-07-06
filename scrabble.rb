c01bb6c50197f8686ab7f7f3b5fe35cec4631a6b
Duluri\Ryan <ryanoftinellb@gmail.com>
2017-07-06 15:06:57 +1000

    Moved letter_score hash to initialise, so it is not redefined each time the 'score' method is run.

--- scrabble.rb
+++ scrabble.rb
@@ -1,5 +1,17 @@
 class Scrabble
 
+	def initialize
+		@@letter_scores = {
+			  "A" => 1, "B" => 3, "C" => 3, "D" => 2,
+			  "E" => 1, "F" => 4, "G" => 2, "H" => 4,
+			  "I" => 1, "J" => 8, "K" => 5, "L" => 1,
+			  "M" => 3, "N" => 1, "O" => 1, "P" => 3,
+			  "Q" =>10, "R" => 1, "S" => 1, "T" => 1,
+			  "U" => 1, "V" => 4, "W" => 4, "X" => 8,
+			  "Y" => 4, "Z" => 10
+			}
+	end
+
 	# determines the Scrabble score for input word
 	# @param word [String] the string to be scored
 	# @param bonus [String] a representation of bonus score modifiers
@@ -11,50 +23,41 @@ class Scrabble
 	# @return [Fixnum] the score of the input word, possibly with bonuses applied
 	def score(word, bonus = nil)
 		if (!word) then return 0 end
-		@@score_hash = {
-			  "A" => 1, "B" => 3, "C" => 3, "D" => 2,
-			  "E" => 1, "F" => 4, "G" => 2, "H" => 4,
-			  "I" => 1, "J" => 8, "K" => 5, "L" => 1,
-			  "M" => 3, "N" => 1, "O" => 1, "P" => 3,
-			  "Q" => 10, "R" => 1, "S" => 1, "T" => 1,
-			  "U" => 1, "V" => 4, "W" => 4, "X" => 8,
-			  "Y" => 4, "Z" => 10
-			}
 		begin
 			if (!bonus)
-				return word.chars.map{|a| @@score_hash[a.capitalize]}.reduce(:+) || 0
+				return word.chars.map{|a| @@letter_scores[a.capitalize]}.reduce(:+) || 0
 			end
 		rescue TypeError
 				raise ArgumentError, "illegal characters in word"
 		end
 		# else if bonus
-		if (word.length < bonus.length) then
+		if (word.length < bonus.length)
 			raise ArgumentError, "too many characters in bonus string"
-		elsif (word.length > bonus.length) then
+		elsif (word.length > bonus.length)
 			raise ArgumentError, "too few characters in bonus string"
 		end
-		@out = 0
-		@word_multiplier = 1
+		out = 0
+		word_multiplier = 1
 		word.chars.zip(bonus.chars).each do |a|
-			@temp = @@score_hash[a[0].capitalize]
-			if !@temp
+			temp = @@letter_scores[a[0].capitalize]
+			if !temp
 				raise ArgumentError, "illegal characters in word"
 			end
 			case a[1]
 				when "-"
-					@out += @temp
+					out += temp
 				when "2", "3"
-					@out += a[1].to_i * @temp
+					out += a[1].to_i * temp
 				when "D"
-					@out += @temp
-					@word_multiplier *= 2
+					out += temp
+					word_multiplier *= 2
 				when "T"
-					@out += @temp
-					@word_multiplier *= 3
+					out += temp
+					word_multiplier *= 3
 				else
 					raise ArgumentError, "illegal characters in bonus string"
-			end	
+			end
 		end
-		return @out *= @word_multiplier
+		return out *= word_multiplier
 	end
 end
